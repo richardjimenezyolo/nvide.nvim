a2a08020c6d53e236398a56adb09979b40930ff1
Richard Jimenez <richardjimenez176@gmail.com>
2025-05-24 20:37:01 -0400

    V2

--- init.lua
+++ init.lua
@@ -1,124 +1,210 @@
-require("plugins")
-require("keymaps")
-require("config")
-require("debugger")
+ local ensure_packer =function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
 
-require("mason").setup()
-require("mason-lspconfig").setup()
+local packer_bootstrap = ensure_packer()
 
--- empty setup using defaults
-require("nvim-tree").setup({
-  view = {
-    adaptive_size = true,
-    side = "left",
-    width = 30,
-  },
-})
+require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use 'catppuccin/vim'
+	use 'rose-pine/neovim'
+	use 'nvim-tree/nvim-tree.lua'
+	use 'nvim-treesitter/nvim-treesitter'
+	use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
 
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "rust" },
+	use 'mason-org/mason.nvim'
+	use { 'mason-org/mason-lspconfig.nvim', requires = { { 'neovim/nvim-lspconfig' } } }
+	use "stevearc/conform.nvim"
+	use "zapling/mason-conform.nvim"
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use 'tpope/vim-dadbod'
+	use 'kristijanhusak/vim-dadbod-ui'
+	use 'kristijanhusak/vim-dadbod-completion'
 
-  sync_install = false,
-  auto_install = true,
+	use 'j-hui/fidget.nvim'
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+	use 'editorconfig/editorconfig-vim'
 
-  incremental_selection = {
-  enable = true,
-    keymaps = {
-      init_selection = "<C-Space>", -- set to `false` to disable one of the mappings
-      node_incremental = "<C-Space>"
-      -- scope_incremental = "grc",
-      -- node_decremental = "grm",
-    },
-  },
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-  disable = function(lang, buf)
-    local max_filesize = 100 * 1024 -- 100 KB
-    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    if ok and stats and stats.size > max_filesize then
-      return true
-    end
-  end,
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
 
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  }
-}
+	use {
+		"NeogitOrg/neogit",
+		requires = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
 
--- NOTE: This line set the backgroud transparent
--- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+		}
+	}
 
-
-local lsp = require('lsp-zero').preset({})
-
-lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-lsp.setup()
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
 
-
-local neogit = require('neogit')
-neogit.setup {}
-
-require('vgit').setup()
-
-
-require("bufferline").setup {
-  options = {
-    hover = {
-      enabled = true,
-      delay = 100,
-      reveal = { 'close' }
-    },
-    numbers = 'buffer_id',
-    color_icons = true,
-    diagnostics = "nvim_lsp",
-    separator_style = 'padded_slant',
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local icon = level:match("error") and " " or " "
-      return " " .. icon .. count
-    end
-  }
-}
-
--- require "fidget".setup {}
-
-require('onedark').setup {
-  style = 'deep'
-}
-require('onedark').load()
+-- empty setup using defaults
+require("nvim-tree").setup()
 
 require('lualine').setup()
+require("mason").setup()
+require("mason-lspconfig").setup()
+require('fidget').setup()
 
-require('config-local').setup {
-  -- Default configuration (optional)
-  config_files = { ".vimrc.lua", ".vimrc" }, -- Config file patterns to load (lua supported)
-  hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
-  autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
-  commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
-  silent = false, -- Disable plugin messages (Config loaded/ignored)
-  lookup_parents = false, -- Lookup config files in parent directories
-}
+vim.opt.termguicolors = true
+require("bufferline").setup()
+
+require("mason-conform").setup()
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('user_lsp_attach', { clear = true }),
+	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+		vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+		vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+		vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+		vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+		vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+		vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+		vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+		vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+	end,
+})
 
 local cmp = require('cmp')
 
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-  })
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+			-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+			-- For `mini.snippets` users:
+			-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+			-- insert({ body = args.body }) -- Insert at cursor
+			-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+			-- require("cmp.config").set_onetime({ sources = {} })
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' }, -- For vsnip users.
+		{ name = 'vim-dadbod-completion' }
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
 })
+
+require 'nvim-treesitter.configs'.setup {
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+
+	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+	highlight = {
+		enable = true,
+
+		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+		-- the name of the parser)
+		-- list of language that will be disabled
+		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+}
+
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
+
+vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-c>', ':q<CR>')
+vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('n', '<C-e>', ':Telescope buffers<CR>')
+vim.keymap.set('n', '<C-p>', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<C-f>', ':Format<CR>')
+vim.keymap.set('n', "<space>s", ":Telescope lsp_workspace_symbols<CR>")
+vim.keymap.set('n', "<C-O>", ":Telescope lsp_document_symbols<CR>")
+vim.keymap.set('v', "<", "<gv")
+vim.keymap.set('v', ">", ">gv")
+
+vim.cmd("colorscheme rose-pine-moon")
+vim.cmd("set number")
