94f3e3ab22b97c71ac9e9396555b1904032707e2
Richard Jimenez <richardjimenez176@gmail.com>
2025-05-26 02:02:12 -0400

    find file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
 vim.keymap.set('n', "<leader>ve", ":e ~/.config/nvim/init.lua<CR>")
-vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', "<C-b>", ":NvimTreeFindFileToggle<CR>")
 vim.keymap.set('n', '<C-c>', ':q<CR>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 vim.keymap.set('i', "<C-s>", "<Esc>:wa<CR>")
